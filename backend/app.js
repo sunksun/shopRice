26b6530334a12becb25e7df6591c163ef4ac41a5
sunksun <sunksunlapunt@gmail.com>
2023-11-20 16:30:20 +0700

    v1.0.0

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,11 @@ import dotenv from 'dotenv'
 
 dotenv.config({ path: 'backend/config/config.env'});
 
-console.log("hello")
+// Import all routes
+import productRoutes from "./routes/products.js";
+
+app.use("/api/v1", productRoutes);
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server started on PORT: ${process.env.PORT}`);
-});
+    console.log(`Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`);
+}); 
