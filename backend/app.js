cdf4f1ec1a57c5b7aa7706a82bb6282c2b548400
sunksun <sunksunlapunt@gmail.com>
2023-11-26 08:47:50 +0700

    26-11-2566

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,12 @@
 import express from "express"; 
 const app = express();
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
+import { connectDatabase } from "./config/dbConnect.js";
 
 dotenv.config({ path: 'backend/config/config.env'});
+
+// Connecting to Database
+connectDatabase();
 
 // Import all routes
 import productRoutes from "./routes/products.js";
@@ -10,6 +14,8 @@ import productRoutes from "./routes/products.js";
 app.use("/api/v1", productRoutes);
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`);
+    console.log(
+        `Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
+        );
 }); 
 // test pull
